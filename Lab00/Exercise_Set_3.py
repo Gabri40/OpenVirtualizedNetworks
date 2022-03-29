dd8d24db1578615eb4d4013358b0d638eda833e9
Gabriele <s262459@studenti.polito.it>
2022-03-29 11:44:05 +0200

    smashing head on virtual evns

--- Lab00/Exercise_Set_3.py
+++ Lab00/Exercise_Set_3.py
@@ -1,5 +1,7 @@
 # block comment/uncomment for specifics exercise set points
 
 import numpy as np
-
+import mathplotlib as mpl
+import mathplotlib.pyplot as plt
 # 1. Create a 4X2 integer array and print its att
+
