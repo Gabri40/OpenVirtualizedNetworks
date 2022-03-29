fc914aea321e0ac48c6cf716123bc97e32f713dd
Gabriele <s262459@studenti.polito.it>
2022-03-29 12:02:40 +0200

    got venv orking with wsl and pycharm

--- Lab00/Exercise_Set_3.py
+++ Lab00/Exercise_Set_3.py
@@ -1,7 +1,8 @@
 # block comment/uncomment for specifics exercise set points
 
 import numpy as np
-import mathplotlib as mpl
-import mathplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 # 1. Create a 4X2 integer array and print its att
 
